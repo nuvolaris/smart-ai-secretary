00cff8c71806b3ae6bc9d86ef377a877de4c43af
Antonio Piga <antonio.piga93@gmail.com>
2023-12-01 19:31:25 +0100

    feature: home start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 export default {
-	content: [],
-	theme: {
-		extend: {}
-	},
-	plugins: []
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      primary: "#1b81a5",
+      secondary: "#ffa944",
+    },
+    fontFamily: {
+      arista: ["Courier New", "monospace"]
+    },
+    extend: {}
+  },
+  plugins: []
 };

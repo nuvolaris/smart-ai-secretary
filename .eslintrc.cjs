8e12920b228e1b8c5cea2ac402a3782c1ac2cdc8
Antonio Piga <antonio.piga93@gmail.com>
2023-12-03 13:44:38 +0100

    fix: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,30 @@
-/** @type { import("eslint").Linter.FlatConfig } */
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+    // ...
+    extends: ['plugin:svelte/recommended'],
+    // ...
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.svelte'] // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+    },
+    env: {
+        es6: true,
+        browser: true
+    },
+    overrides: [
+        {
+            files: ['*.svelte'],
+            parser: 'svelte-eslint-parser',
+            // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+            parserOptions: {
+                parser: '@typescript-eslint/parser'
+            }
+        }
+        // ...
+    ]
+    // ...
 };
+

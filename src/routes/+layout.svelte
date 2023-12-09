b2e2c153f212b3513e514e61d0d6812f8d1c2f41
Antonio Piga <antonio.piga93@gmail.com>
2023-12-09 14:48:59 +0100

    featur: christmas header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '../app.css';
-	import Header from '$lib/components/Header.svelte';
+	//import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import HeaderChristmas from '$lib/components/HeaderChristmas.svelte';
 </script>
 
-<Header />
+<HeaderChristmas />
 <div class="p-1 mb-10 min-h-screen">
 	<slot />
 </div>

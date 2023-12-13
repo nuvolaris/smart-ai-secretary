9e7f306a24302e7d2529a2ad10e19373cf875e3c
Antonio Piga <antonio.piga93@gmail.com>
2023-12-13 20:36:53 +0100

    refactor: icons on header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
-	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Header />
-<div class="p-1 mb-10 min-h-screen">
+<div class="p-1 mb-10">
 	<slot />
 </div>
-<Footer />
